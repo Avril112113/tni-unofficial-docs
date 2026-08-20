42178613149d857745d6ed9f7370eeed690fed9e
Avril112113 <25151112+Avril112113@users.noreply.github.com>
2026-08-20 19:45:27 +0100

    Stuff to local serve https

--- test_serve.bat
+++ test_serve.bat
@@ -1,2 +1,6 @@
 @echo off
-bundle exec jekyll serve --livereload --watch --incremental --host=0.0.0.0
+
+@REM openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout localhost.key -out localhost.crt
+@REM bundler exec jekyll serve --watch --incremental --host=0.0.0.0 --ssl-key localhost.key --ssl-cert localhost.crt
+
+bundler exec jekyll serve --livereload --watch --incremental --host=0.0.0.0
